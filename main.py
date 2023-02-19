cd8b1186f1c59c42137a69b6c75db40ba58208fb
mardenlelis <marden.n.lelis@gmail.com>
2023-02-19 10:56:36 -0300

    Alterando saída de valor gasto

---
+++ main.py
@@ -0,0 +1,36 @@
+from calculo import Calculo
+
+def main():
+    print(
+        """
+        Esta aplicação tem como finalidade demonstrar os valores que serão gastos 
+        com combustível durante uma viagem, com base no consumo do seu veículo, e 
+        com a distância determinada por você!        
+        """
+    )
+
+    print("Os combustíveis disponíveis para este cálculo são:")
+    print("    ° Álcool")
+    print("    ° Díesel")
+    print("    ° Gasolina")
+
+    print(" ")
+
+    try:
+        distancia = float(input('Qual a distância em Quilômetros a ser percorrida?'))
+        consumo = float(input('Qual o consumo em litros por Kilometro percorrido?'))
+
+        calculo = Calculo()
+
+        print(
+            calculo.calcular_gasto(distancia, consumo)
+        )
+    except ValueError as erro:
+        print('O valor recebido não é válido')
+
+
+if __name__ == '__main__':
+    main()
+
+
+
